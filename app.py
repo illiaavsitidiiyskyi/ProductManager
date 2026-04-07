64e55ff32266a9dc0168827c821b51ce2f18fc28
illiaavsitidiiyskyi <iavsitidiiskyi@gmail.com>
2026-04-07 20:03:09 +0200

    created db

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-
+from models import init_db
+from action_db import *
 app = Flask(__name__)
 app.secret_key = '123'
-products = [
-    {'name': 'phone', 'price': 3000, 'category': 'device'},
-    {'name': 'notebook', 'price': 100, 'category': 'school'}
-]
+init_db()
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
